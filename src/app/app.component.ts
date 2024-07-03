0c35c9656d794de5f3bb494d9faa0526ecb29f8c
p.kosc <p.kosc@resolution.de>
2024-07-03 18:03:36 +0200

    added input demo

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from './header.component';
+import {UserComponent} from './user.component';
+import {DUMMY_USERS} from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    UserComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+}
